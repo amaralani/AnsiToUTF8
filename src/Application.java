59ac7d9772070fc7bb614a922cde46c1fe3c7293
Amir <amir.maralani@gmail.com>
2022-02-09 09:27:42 +0330

    Fix for no-arg

--- src/Application.java
+++ src/Application.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) {
-        if ("no-ui".equals(args[0])) {
+        if (args.length > 0 && "no-ui".equals(args[0])) {
             try {
                 CustomFileConverter.createFile(args[1], args[1].substring(0, args[1].length() - 4) + "UTF8" + args[1].substring(args[1].length() - 4));
             } catch (IOException e) {
